fabf7a5547956446be04448b05b42b32b59cd1bc
McNeelinet <lavelin.sava@gmail.com>
2021-06-11 01:16:02 +0300

    final

--- components/footer.js
+++ components/footer.js
@@ -5,8 +5,8 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div>
                 <h1>Контакты:</h1>
-                <a href="" target="_blank"><img src=""></img></a>
-                <a href="" target="_blank"><img src=""></img></a>
+                <a href="https://t.me/McNeelinet" target="_blank"><img src={"/tg.png"}></img></a>
+                <a href="https://vk.com/mcneelinet" target="_blank"><img src={"/vk.png"}></img></a>
             </div>
         </footer>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/blog.module.css'
 
 import Footer from '../components/footer'
 import Header from '../components/header'
@@ -17,6 +17,20 @@ export default function About() {
       <Header>
       </Header>
       <main className={styles.main}>
+        <div className={styles.article}>
+          <div className={styles.postInfo}>
+            <h1>Добро пожаловать</h1>
+          </div>
+          <div className={styles.postContent}>
+            <p>Проект подготовлен Лавелиным С.П.</p>
+            <p>Сегодня, во времена искаженной системы оценок, которая теперь работает не по шкале от 1 до 10, а от 5 до 10, во времена, когда на рейтинг фильма влияет количество засланных продажным изданиям условных "чемоданов", доверять можно лишь
+              небольшому количеству независимых обзорщиков. 
+            </p>
+            <p>
+              Проект "Честный обзорщик" является как раз таким. На нем размещаются обзоры от меня и моих товарищей, которые, как и я, являются любителями хорошего кино и сериалов.
+            </p>
+          </div>
+        </div>
       </main>
       <Footer>
       </Footer>
